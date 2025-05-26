beab5241267792d37bfc408955218866d1a14219
Ana Belén García Añón <agarciaanon@gmail.com>
2025-05-26 08:32:37 +0200

    Exercise20 solved.

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,18 +6,23 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("John", "Smith"));
+        people.add(new Teacher("María", "Montessori", "Educación"));
+        people.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        people.add(new Doctor("Gregory ", "House", "Nefrología e infectología"));
 
+        return people;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (Person p : stringList) {
+            p.getDetails();
+        }
     }
 
     public static void main(String[] args) {
         List<Person> stringList = getPeopleList();
         showPeopleDetails(stringList);
     }
-
-
-
 }
